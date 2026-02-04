686b59e47078724d2500fe835f7ad7cf360b2a71
Keshav Lathi <163568276+Keshavlathi@users.noreply.github.com>
2026-02-05 00:00:57 +0530

    Add files via upload

---
+++ table.py
@@ -0,0 +1,12 @@
+# num = int(input("Enter the number:"))
+
+# for i in range(1,11):
+    
+#     print(f"{num}*{i}={num*i}")
+   
+num = int(input("Enter the number:"))
+i = 1
+
+while(i<11):
+    print(f"{num}*{i}={num*i}")
+    i+=1
